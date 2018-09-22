cfebd76a76f25a8bdb5222bddba67cfdd868c775
howyi <qrbys@outlook.com>
2018-09-22 22:58:43 +0900

    add APIs

--- src/ClickUp/Client.php
+++ src/ClickUp/Client.php
@@ -2,17 +2,116 @@
 
 namespace ClickUp;
 
+use ClickUp\Objects\SpaceCollection;
+use ClickUp\Objects\Team;
+use ClickUp\Objects\TeamCollection;
+use ClickUp\Objects\User;
+
 class Client
 {
-	public function __construct()
+	private $guzzleClient;
+
+	public function __construct($token)
+	{
+		$this->guzzleClient = new \GuzzleHttp\Client([
+			'base_uri' => 'https://api.clickup.com/api/v1/',
+			'headers' => [
+				'Authorization' => $token,
+			]
+		]);
+	}
+
+	/**
+	 * @return User
+	 */
+	public function user()
+	{
+		return new User(
+			$this,
+			$this->get('user')['user']
+		);
+	}
+
+	/**
+	 * @return TeamCollection
+	 */
+	public function teams()
+	{
+		return new TeamCollection(
+			$this,
+			$this->get('team')['teams']
+		);
+	}
+
+	/**
+	 * @param $teamId
+	 * @return Team
+	 */
+	public function team($teamId)
+	{
+		return new Team(
+			$this,
+			$this->get("team/$teamId")['team']
+		);
+	}
+
+	public function space($teamId)
+	{
+		return new SpaceCollection(
+			$this,
+			$this->get("team/$teamId/space")['spaces']
+		);
+	}
+
+	public function projects($spaceId)
 	{
 	}
 
-	public function authenticate()
+	public function createList($projectId, $body)
 	{
 	}
 
-	public function api()
+	public function editList($projectId, $body)
 	{
+	}
+
+	public function getTasks($teamId, $params)
+	{
+	}
+
+	public function createTask($listId, $body)
+	{
+	}
+
+	public function editTask($taskId, $body)
+	{
+	}
+
+	/**
+	 * @param string $method
+	 * @param array  $params
+	 * @return mixed
+	 */
+	public function get($method, $params = [])
+	{
+		dump("request : $method");
+		return \GuzzleHttp\json_decode($this->guzzleClient->request('GET', $method, ['json' => $params])->getBody(), true);
+	}
+
+	public function post($method, $body = [])
+	{
+	}
+
+	public function put($method, $body = [])
+	{
+	}
+
+	public function delete($method, $params = [])
+	{
+	}
+
+	public function guzzleClient()
+	{
+		return $this->guzzleClient;
 	}
 }
